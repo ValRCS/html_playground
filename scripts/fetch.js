8ab86c96bb691961fdd927f70e9cf900814df0b3
Valdis Saules <valdis.s.coding@gmail.com>
2019-08-02 12:23:49 +0300

    Added example of how to add a single li

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,9 +1,20 @@
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
+
+
 function processJSON(json) {
     console.log("Processing some json");
     // console.log(json);
     console.log(json[0]);
+    const ul = document.createElement("ol");
+    //loop goes here
+    //we add each li element here
+    const li = document.createElement("li");
+    li.innerText = json[0]['title'];
+    ul.appendChild(li);
+
+    //we should end loop here after everything is added
+    document.querySelector("main").appendChild(ul);
 }
 
 
